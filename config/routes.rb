581c29342e5ef8ada4459cdadc3cd54b32893c61
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-20 22:33:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
